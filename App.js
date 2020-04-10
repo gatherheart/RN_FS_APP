850d7c2302543b68994fcc25ae6ad9e3ce8a699d
gatherheart <imonit@naver.com>
2020-04-11 00:17:40 +0900

    [ADD] Preloading and Apploading configuration

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Image, Text, View } from "react-native";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  // For checking if app is loading or not
+  const [loaded, setLoaded] = useState(false);
+
+  // Prefeching Image Cache
+  const cacheImages = (images) =>
+    images.map((image) => {
+      if (typeof image === "string") {
+        return Image.prefetch(image);
+      } else {
+        return Asset.fromModule(image).downloadAsync();
+      }
+    });
+  // Prefetching Font Cache
+  const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+  const loadAssets = () => {
+    const images = cacheImages([
+      require("./assets/splash.png"),
+      require("./assets/icon.png"),
+    ]);
+
+    const fonts = cacheFonts([Ionicons.font, FontAwesome.font]);
+    return Promise.all([...images, ...fonts]);
+  };
+  const preload = async () => {
+    try {
+      await loadAssets();
+    } catch (e) {
+      console.log(e);
+    }
+    setLoaded(true);
+  };
+
+  useEffect(() => {
+    preload();
+  }, []);
+  return loaded ? (
+    <View>
+      <Text>"Hello World"</Text>
     </View>
+  ) : (
+    <AppLoading></AppLoading>
   );
 }
 
