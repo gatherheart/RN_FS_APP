96beaedbbcbef6d8336c8309b80c3ef21e5b97c3
gatherheart <imonit@naver.com>
2020-05-07 17:14:46 +0900

    [ADD] Custom Image and design

--- screens/Group/GroupPresenter.js
+++ screens/Group/GroupPresenter.js
@@ -19,6 +19,12 @@ const { width, height } = Dimensions.get("screen");
 
 const thumbMeasure = (width - 48 - 32) / 3;
 
+const backgroundImg =
+  "https://images.unsplash.com/photo-1588780530902-bbc23735248c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=650";
+
+const profileImg =
+  "https://images.unsplash.com/photo-1588785392665-f6d4a541417d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=320";
+
 export default ({ id, groupName, loading, refreshFn }) => {
   const navigation = useNavigation();
   navigation.setOptions({
@@ -28,20 +34,17 @@ export default ({ id, groupName, loading, refreshFn }) => {
     <Block flex style={styles.profile}>
       <Block flex>
         <ImageBackground
-          source={Images.ProfileBackground}
+          source={{ uri: backgroundImg }}
           style={styles.profileContainer}
           imageStyle={styles.profileBackground}
         >
           <ScrollView
             showsVerticalScrollIndicator={false}
-            style={{ width, marginTop: "25%" }}
+            style={{ width, paddingTop: "25%" }}
           >
             <Block flex style={styles.profileCard}>
               <Block middle style={styles.avatarContainer}>
-                <Image
-                  source={{ uri: Images.ProfilePicture }}
-                  style={styles.avatar}
-                />
+                <Image source={{ uri: profileImg }} style={styles.avatar} />
               </Block>
               <Block style={styles.info}>
                 <Block
@@ -169,149 +172,53 @@ export default ({ id, groupName, loading, refreshFn }) => {
                     ))}
                   </Block>
                 </Block>
+                <Block style={{ paddingBottom: -HeaderHeight * 2 }}>
+                  <Block row space="between" style={{ flexWrap: "wrap" }}>
+                    {Images.Viewed.map((img, imgIndex) => (
+                      <Image
+                        source={{ uri: img }}
+                        key={`viewed-${img}`}
+                        resizeMode="cover"
+                        style={styles.thumb}
+                      />
+                    ))}
+                  </Block>
+                </Block>
               </Block>
             </Block>
           </ScrollView>
         </ImageBackground>
       </Block>
-      {/* <ScrollView showsVerticalScrollIndicator={false} 
-                  contentContainerStyle={{ flex: 1, width, height, zIndex: 9000, backgroundColor: 'red' }}>
-      <Block flex style={styles.profileCard}>
-        <Block middle style={styles.avatarContainer}>
-          <Image
-            source={{ uri: Images.ProfilePicture }}
-            style={styles.avatar}
-          />
-        </Block>
-        <Block style={styles.info}>
-          <Block
-            middle
-            row
-            space="evenly"
-            style={{ marginTop: 20, paddingBottom: 24 }}
-          >
-            <Button small style={{ backgroundColor: argonTheme.COLORS.INFO }}>
-              CONNECT
-            </Button>
-            <Button
-              small
-              style={{ backgroundColor: argonTheme.COLORS.DEFAULT }}
-            >
-              MESSAGE
-            </Button>
-          </Block>
-
-          <Block row space="between">
-            <Block middle>
-              <Text
-                bold
-                size={12}
-                color="#525F7F"
-                style={{ marginBottom: 4 }}
-              >
-                2K
-              </Text>
-              <Text size={12}>Orders</Text>
-            </Block>
-            <Block middle>
-              <Text bold size={12} style={{ marginBottom: 4 }}>
-                10
-              </Text>
-              <Text size={12}>Photos</Text>
-            </Block>
-            <Block middle>
-              <Text bold size={12} style={{ marginBottom: 4 }}>
-                89
-              </Text>
-              <Text size={12}>Comments</Text>
-            </Block>
-          </Block>
-        </Block>
-        <Block flex>
-            <Block middle style={styles.nameInfo}>
-              <Text bold size={28} color="#32325D">
-                Jessica Jones, 27
-              </Text>
-              <Text size={16} color="#32325D" style={{ marginTop: 10 }}>
-                San Francisco, USA
-              </Text>
-            </Block>
-            <Block middle style={{ marginTop: 30, marginBottom: 16 }}>
-              <Block style={styles.divider} />
-            </Block>
-            <Block middle>
-              <Text size={16} color="#525F7F" style={{ textAlign: "center" }}>
-                An artist of considerable range, Jessica name taken by
-                Melbourne …
-              </Text>
-              <Button
-                color="transparent"
-                textStyle={{
-                  color: "#233DD2",
-                  fontWeight: "500",
-                  fontSize: 16
-                }}
-              >
-                Show more
-              </Button>
-            </Block>
-            <Block
-              row
-              style={{ paddingVertical: 14, alignItems: "baseline" }}
-            >
-              <Text bold size={16} color="#525F7F">
-                Album
-              </Text>
-            </Block>
-            <Block
-              row
-              style={{ paddingBottom: 20, justifyContent: "flex-end" }}
-            >
-              <Button
-                small
-                color="transparent"
-                textStyle={{ color: "#5E72E4", fontSize: 12 }}
-              >
-                View all
-              </Button>
-            </Block>
-            <Block style={{ paddingBottom: -HeaderHeight * 2 }}>
-              <Block row space="between" style={{ flexWrap: "wrap" }}>
-                {Images.Viewed.map((img, imgIndex) => (
-                  <Image
-                    source={{ uri: img }}
-                    key={`viewed-${img}`}
-                    resizeMode="cover"
-                    style={styles.thumb}
-                  />
-                ))}
-              </Block>
-            </Block>
-        </Block>
-      </Block>
-                </ScrollView>*/}
     </Block>
   );
 };
 
 const styles = StyleSheet.create({
   profile: {
+    borderColor: "black",
+    borderWidth: 1,
     marginTop: Platform.OS === "android" ? -HeaderHeight : 0,
     // marginBottom: -HeaderHeight * 2,
     flex: 1,
   },
   profileContainer: {
+    borderColor: "black",
+    borderWidth: 1,
     width: width,
     height: height,
     padding: 0,
     zIndex: 1,
   },
   profileBackground: {
+    borderColor: "black",
+    borderWidth: 1,
     width: width,
     height: height / 2,
   },
   profileCard: {
     // position: "relative",
+    borderColor: "black",
+    borderWidth: 1,
     padding: theme.SIZES.BASE,
     marginHorizontal: theme.SIZES.BASE,
     marginTop: 65,
@@ -325,19 +232,27 @@ const styles = StyleSheet.create({
     zIndex: 2,
   },
   info: {
+    borderColor: "black",
+    borderWidth: 1,
     paddingHorizontal: 40,
   },
   avatarContainer: {
+    borderColor: "black",
+    borderWidth: 1,
     position: "relative",
     marginTop: -80,
   },
   avatar: {
+    borderColor: "black",
+    borderWidth: 1,
     width: 124,
     height: 124,
     borderRadius: 62,
     borderWidth: 0,
   },
   nameInfo: {
+    borderColor: "black",
+    borderWidth: 1,
     marginTop: 35,
   },
   divider: {
@@ -346,6 +261,8 @@ const styles = StyleSheet.create({
     borderColor: "#E9ECEF",
   },
   thumb: {
+    borderColor: "black",
+    borderWidth: 1,
     borderRadius: 4,
     marginVertical: 4,
     alignSelf: "center",
