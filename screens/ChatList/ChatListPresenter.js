11f18ec7d9f9a283815a91a296ede1b97081a904
gatherheart <imonit@naver.com>
2020-07-20 23:40:41 +0900

    [ADD] Custom Scroll Tab Bar added

--- screens/ChatList/ChatListPresenter.js
+++ screens/ChatList/ChatListPresenter.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { HeaderHeight } from "../../utils/HeaderHeight";
+import { HeaderHeight, UnderHeader } from "../../utils/HeaderHeight";
 import ChatListHeader from "../../components/Chat/ChatListHeader";
 import { useNavigation } from "@react-navigation/native";
 import ScrollableTabView, {
   ScrollableTabBar,
 } from "react-native-scrollable-tab-view";
+import { trimText } from "../../utils/String";
+import FacebookTabBar from "./FacebookTabBar";
+import {
+  GREEN_COLOR,
+  BG_COLOR,
+  GREY_COLOR,
+  LIGHT_GREEN_COLOR,
+} from "../../constants/Color";
+import CustomTabBar from "./CustomTabBar";
 
 export default ({ rooms }) => {
   const navigation = useNavigation();
@@ -14,15 +23,43 @@ export default ({ rooms }) => {
     <>
       <ChatListHeader></ChatListHeader>
       <ScrollableTabView
-        style={{ marginTop: 20 }}
+        style={{ paddingTop: HeaderHeight }}
         initialPage={0}
-        renderTabBar={() => <ScrollableTabBar />}
+        renderTabBar={() => (
+          <CustomTabBar
+            tabStyle={{
+              backgroundColor: LIGHT_GREEN_COLOR,
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+              paddingHorizontal: 10,
+            }}
+            activeTextColor={BG_COLOR}
+            inactiveTextColor={GREY_COLOR}
+            tabUnderlineStyle={{ borderColor: BG_COLOR }}
+          />
+        )}
       >
-        <Text tabLabel="Tab #1">My</Text>
-        <Text tabLabel="Tab #2 word word">favorite</Text>
-        <Text tabLabel="Tab #3 word word word">project</Text>
-        <Text tabLabel="Tab #4 word word word word">favorite</Text>
-        <Text tabLabel="Tab #5">project</Text>
+        <View tabLabel={`${trimText("ios-arrow-forward", 20)}`}>
+          <Text>My</Text>
+        </View>
+        <View tabLabel={`${trimText("ios-arrow-forward", 20)}`}>
+          <Text>My</Text>
+        </View>
+        <View tabLabel={`${trimText("ios-arrow-forward", 20)}`}>
+          <Text>My</Text>
+        </View>
+        <View tabLabel={`${trimText("ios-arrow-forward", 20)}`}>
+          <Text>My</Text>
+        </View>
+        <View tabLabel={`${trimText("ios-arrow-forward", 20)}`}>
+          <Text>My</Text>
+        </View>
+        <View tabLabel={`${trimText("ios-arrow-forward", 20)}`}>
+          <Text>My</Text>
+        </View>
+        <View tabLabel={`${trimText("ios-arrow-forward", 20)}`}>
+          <Text>My</Text>
+        </View>
       </ScrollableTabView>
     </>
   );
